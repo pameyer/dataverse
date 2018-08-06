e0e2f10d48839f6760398919be0528d73f507e1f
Pete Meyer <pameyer@crystal.harvard.edu>
2018-08-05 23:33:08 -0400

    browse to login page

--- src/test/java/edu/harvard/iq/dataverse/api/BrowserIT.java
+++ src/test/java/edu/harvard/iq/dataverse/api/BrowserIT.java
@@ -5,6 +5,8 @@ import org.junit.BeforeClass;
 import org.junit.AfterClass;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import static junit.framework.Assert.assertEquals;
 
@@ -35,6 +37,20 @@ public class BrowserIT
 		//System.out.format("baseURI=%s\n",RestAssured.baseURI);
 		driver.get(RestAssured.baseURI);
 		String title_expected = "Root";
+		String title_observed = driver.getTitle();
+		assertEquals( title_expected, title_observed);
+	}
+
+	/**
+	 * try to login in.
+	 * Eventually - for now, just try to load the login page
+	 */
+	@Test
+	public void testLogin()
+	{
+		WebElement element = driver.findElement(By.id("loginpage-header"));
+		element.click();
+		String title_expected = "Log In - Root";
 		String title_observed = driver.getTitle();
 		assertEquals( title_expected, title_observed);
 	}
