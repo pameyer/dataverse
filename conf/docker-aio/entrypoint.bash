ebe50c4f2686b9e57373208c69e5bb72df1e6b93
Pete Meyer <pameyer@crystal.harvard.edu>
2019-01-15 11:22:28 -0500

    cleanup

--- conf/docker-aio/entrypoint.bash
+++ conf/docker-aio/entrypoint.bash
@@ -3,14 +3,12 @@ export LANG=en_US.UTF-8
 #sudo -u postgres /usr/bin/postgres -D /var/lib/pgsql/data &
 sudo -u postgres /usr/pgsql-9.6/bin/postgres -D /var/lib/pgsql/data &
 cd /opt/solr-7.3.1/
-# TODO: Run Solr as non-root and remove "-force".
 sudo -u solr bin/solr start 
 sudo -u solr bin/solr create_core -c collection1 -d server/solr/collection1/conf 
 
 # start apache, in both foreground and background...
 apachectl -DFOREGROUND &
 
-# TODO: Run Glassfish as non-root.
 cd /opt/glassfish4
 sudo -u glassfish bin/asadmin start-domain --debug
 sleep infinity
